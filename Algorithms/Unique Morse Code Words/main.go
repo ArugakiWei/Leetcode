ab76310e2e641928592126b4c9a7b4b025bad44e
arugakiWei <arugaki.wei@daocloud.io>
2018-05-13 16:31:56 +0800

    fix

--- Algorithms/Unique Morse Code Words/main.go
+++ Algorithms/Unique Morse Code Words/main.go
@@ -4,18 +4,26 @@ import "fmt"
 
 func main() {
 
-	foo := []string{".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."}
-	bar := make(map[byte]string)
-	for i, v := range foo{
-		bar[97+byte(i)] = v
-	}
-	for i, k := range bar {
-		fmt.Println(string(i), k)
-	}
+	b := []string{"zocd","gjkl","hzqk","hzgq","gjkl"}
+	fmt.Println(uniqueMorseRepresentations(b))
 }
 
 
 func uniqueMorseRepresentations(words []string) int {
 
+	foo := []string{".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."}
+	bar := make(map[byte]string)
+	for i, v := range foo{
+		bar[97+byte(i)] = v
+	}
+	mcode := make(map[string]interface{})
+	for _, word := range words {
+		cur := ""
+		for _, w := range []byte(word) {
+			cur = cur + bar[w]
+		}
+		mcode[cur]=new(interface{})
+	}
+	return len(mcode)
 }
 
