fe61fa0ab43f3ce42a6acf4c576029fbb795ea24
arugakiWei <arugaki.wei@daocloud.io>
2018-07-29 19:32:04 +0800

    fix

---
+++ Algorithms/Number Complement/main,go.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+
+	a := 5
+	fmt.Println(findComplement(a))
+}
+
+func findComplement(num int) int {
+
+	result := 0
+	for i, v := range toBinaryComplementNumber(num) {
+		result += int(float64(v-48) * math.Pow(2, float64(i)))
+	}
+	return result
+}
+
+func toBinaryComplementNumber(num int) string {
+
+	binary := ""
+	for {
+		if num/2 == 0 {
+			binary += "0"
+			return binary
+		}
+		if num%2 == 0 {
+			binary += "1"
+		}else{
+			binary += "0"
+		}
+		num = num/2
+	}
+	return ""
+}
