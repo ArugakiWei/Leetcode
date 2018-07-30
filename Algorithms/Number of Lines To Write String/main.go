055e2d7f7b39b641b996b35796fd10c7a86c478f
arugakiWei <arugaki.wei@daocloud.io>
2018-07-30 20:48:35 +0800

    fix

---
+++ Algorithms/Number of Lines To Write String/main.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println(numberOfLines([]int{4,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10}, "bbbcccdddaaa"))
+}
+
+func numberOfLines(widths []int, S string) []int {
+
+	lineSum := 0
+	lines := 1
+	lastLine := 0
+	for i:=0; i<len(S); i++ {
+		lineSum += widths[S[i]-'a']
+		fmt.Println(lineSum)
+		if lineSum < 100 {
+			lastLine = lineSum
+			continue
+		}
+		if lineSum == 100 {
+			lines++
+			lineSum = 0
+		}
+		if lineSum > 100 {
+			lines++
+			i--
+			lineSum = 0
+		}
+	}
+	return []int{lines, lastLine}
+}
