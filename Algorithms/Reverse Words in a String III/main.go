a35a16dbdd3e1cab592b2e2e2fb611ad8cf65255
arugakiWei <arugaki.wei@daocloud.io>
2018-08-06 22:46:05 +0800

    update

---
+++ Algorithms/Reverse Words in a String III/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+
+	fmt.Println(reverseWords("Let's take LeetCode contest"))
+}
+
+func reverseWords(s string) string {
+
+	var strs []string
+	for _, word := range strings.Split(s, " ") {
+		rw := reverseString(word)
+		strs = append(strs, rw)
+	}
+	return strings.Join(strs, " ")
+}
+
+func reverseString(s string) string {
+
+	var rs []byte
+	for i:=len(s)-1; i>=0; i-- {
+
+		rs = append(rs, s[i])
+	}
+	return string(rs)
+}
