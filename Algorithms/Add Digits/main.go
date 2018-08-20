0e3b3a2efdf3a6f16592d74e20602386b5f38054
arugakiWei <arugaki.wei@daocloud.io>
2018-08-20 20:41:00 +0800

    update

---
+++ Algorithms/Add Digits/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(addDigits(38))
+}
+
+func addDigits(num int) int {
+
+	if num == 0 {
+		return 0
+	}
+	if num%9 == 0 {
+		return 9
+	}else{
+		return num%9
+	}
+}
---
+++ Algorithms/Happy Number/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println(isHappy(19))
+}
+
+func isHappy(n int) bool {
+
+}
