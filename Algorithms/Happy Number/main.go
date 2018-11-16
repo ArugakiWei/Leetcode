b76ef273480b0967169d57f048429cc24ad16155
arugakiWei <arugaki.wei@daocloud.io>
2018-11-16 10:32:13 +0800

    happy number

--- Algorithms/Happy Number/main.go
+++ Algorithms/Happy Number/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"runtime"
+)
 
 func main() {
 
@@ -9,4 +12,84 @@ func main() {
 
 func isHappy(n int) bool {
 
+	runtime.GOMAXPROCS(4)
+
+	slowNum, fastNum := n, n
+	slowCh := make(chan int)
+	fastCh := make(chan int)
+	for  {
+		go slow(slowNum, slowCh)
+		go fast(fastNum, fastCh)
+		for i:=0; i<2; i++{
+			select {
+			case slowNum = <- slowCh :
+			case fastNum = <- fastCh :
+			}
+		}
+		if slowNum == fastNum {
+			break
+		}
+	}
+	if slowNum == 1 {
+		return true
+	}
+	return false
 }
+
+func slow(n int, ch chan int){
+	n = power(n)
+	ch <- n
+}
+
+func fast (n int, ch chan int){
+
+	n = power(n)
+	n = power(n)
+	ch <- n
+}
+
+func power(n int) int {
+	sum := 0
+	for {
+		i := n % 10
+		sum = sum + i*i
+		n = n / 10
+		if n == 0 {
+			break
+		}
+	}
+	return sum
+}
+
+
+/*
+func isHappy(n int) bool {
+
+	slow, fast := n, n
+	for  {
+		slow = power(slow)
+		fast = power(fast)
+		fast = power(fast)
+		if slow == fast {
+			break
+		}
+	}
+	if slow == 1 {
+		return true
+	}
+	return false
+}
+
+func power(n int) int {
+	sum := 0
+	for {
+		i := n % 10
+		sum = sum + i*i
+		n = n / 10
+		if n == 0 {
+			break
+		}
+	}
+	return sum
+}
+ */
