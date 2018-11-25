2993080130316986d18b4af60006a31949929558
arugakiWei <arugaki.wei@daocloud.io>
2018-11-25 14:16:48 +0800

    fix

--- Algorithms/Happy Number/main.go
+++ Algorithms/Happy Number/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"runtime"
 )
 
 func main() {
@@ -10,59 +9,59 @@ func main() {
 	fmt.Println(isHappy(19))
 }
 
-func isHappy(n int) bool {
+//func isHappy(n int) bool {
+//
+//	runtime.GOMAXPROCS(4)
+//
+//	slowNum, fastNum := n, n
+//	slowCh := make(chan int)
+//	fastCh := make(chan int)
+//	for  {
+//		go slow(slowNum, slowCh)
+//		go fast(fastNum, fastCh)
+//		for i:=0; i<2; i++{
+//			select {
+//			case slowNum = <- slowCh :
+//			case fastNum = <- fastCh :
+//			}
+//		}
+//		if slowNum == fastNum {
+//			break
+//		}
+//	}
+//	if slowNum == 1 {
+//		return true
+//	}
+//	return false
+//}
+//
+//func slow(n int, ch chan int){
+//	n = power(n)
+//	ch <- n
+//}
+//
+//func fast (n int, ch chan int){
+//
+//	n = power(n)
+//	n = power(n)
+//	ch <- n
+//}
+//
+//func power(n int) int {
+//	sum := 0
+//	for {
+//		i := n % 10
+//		sum = sum + i*i
+//		n = n / 10
+//		if n == 0 {
+//			break
+//		}
+//	}
+//	return sum
+//}
 
-	runtime.GOMAXPROCS(4)
-
-	slowNum, fastNum := n, n
-	slowCh := make(chan int)
-	fastCh := make(chan int)
-	for  {
-		go slow(slowNum, slowCh)
-		go fast(fastNum, fastCh)
-		for i:=0; i<2; i++{
-			select {
-			case slowNum = <- slowCh :
-			case fastNum = <- fastCh :
-			}
-		}
-		if slowNum == fastNum {
-			break
-		}
-	}
-	if slowNum == 1 {
-		return true
-	}
-	return false
-}
-
-func slow(n int, ch chan int){
-	n = power(n)
-	ch <- n
-}
-
-func fast (n int, ch chan int){
-
-	n = power(n)
-	n = power(n)
-	ch <- n
-}
-
-func power(n int) int {
-	sum := 0
-	for {
-		i := n % 10
-		sum = sum + i*i
-		n = n / 10
-		if n == 0 {
-			break
-		}
-	}
-	return sum
-}
 
 
-/*
 func isHappy(n int) bool {
 
 	slow, fast := n, n
@@ -92,4 +91,3 @@ func power(n int) int {
 	}
 	return sum
 }
- */
