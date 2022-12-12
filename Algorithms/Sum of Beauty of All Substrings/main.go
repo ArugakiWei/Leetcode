afc013ff4340c9b267074a495bbac526a01e400e
arugaki <arugaki.we@gmail.com>
2022-12-12 11:56:25 +0800

    Sum of Beauty of All Substrings

---
+++ Algorithms/Sum of Beauty of All Substrings/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+	// fmt.Println(beautySum("aabcb"))
+	// fmt.Println(beautySum("aabcbaa"))
+	fmt.Println(beautySum("woqrqcvfdgkrafoqdktsfpeygawfpdlvaylgpxhufpvucmmztjoqmxhegdpeczbtvwrmnwrvlptscwwqbjstanyqbgoagxopvgtlyzsemgktcgciualltsquepotmtszbmejbwbtzlavpxnujdsdyrypfcfcfwdidglybzvzuznytwndidzumoekzuukxtpouudsfcohapfcjjmqwjgcvalzarugmzucheydwsncxgyojnfvgroihfckmbtqewxhuqihplprgyeaqhocivaupdfokwpliziwcmuxnebxeszxbsrmffwwdz"))
+}
+
+func beautySum(s string) (ans int) {
+	for i := range s {
+		cnt := [26]int{}
+		for j := i; j < len(s); j++ {
+			cnt[s[j]-'a']++
+			mi, mx := 1000, 0
+			for _, v := range cnt {
+				if v > 0 {
+					if mi > v {
+						mi = v
+					}
+					if mx < v {
+						mx = v
+					}
+				}
+			}
+			ans += mx - mi
+		}
+	}
+	return
+}
